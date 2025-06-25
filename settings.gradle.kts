47ee561574db185119fc0ea113d6bfc6dd07a850
Mudassar786aska <mudassarrajput504@gmail.com>
2025-06-25 19:43:17 +0500

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+import java.net.URI // <-- Add this import at the top of your file
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+
+    }
+}
+
+rootProject.name = "Waves-Of-Food"
+include(":app")
